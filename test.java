bca706432cdad35994c790d919ed5617970b7194
betacompsci <alejandro.garcia@stisd.net>
2014-07-16 23:56:17 -0700

    Test 2

--- test.java
+++ test.java
@@ -2,7 +2,10 @@ public class test{
 
 	public static void main(String[] args)
 	{
-	
+		//This is a test
+		//how do comments come out? 
+		System.out.println("This is a test of the github example");
+		
 	
 	}
 
